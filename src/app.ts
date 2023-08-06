db27838abbb34a339eee6ff422b11caa8f833d3b
Robson-Luna-BR <robson.luna@outlook.com>
2023-08-06 06:57:40 -0300

    adiciona eestrutura de pastas

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import { handleErrors } from "./error";
-
+import { AppDataSource } from "./data-source";
 
 const app = express();
 app.use(express.json());
-
-
 
 app.use(handleErrors);
 
